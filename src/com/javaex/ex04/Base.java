cd3062b3622bc496523606b3e2ff0c8d9eb391ab
yesung <yesung@localhost>
2024-03-25 23:18:31 +0900

    24.03.25 HOME

--- src/com/javaex/ex04/Base.java
+++ src/com/javaex/ex04/Base.java
@@ -1,27 +1,31 @@
 package com.javaex.ex04;
 
 public class Base {
-
+	
+	private String state;
+	
 	public void service(String state) {
-
+		this.service = service;
+		System.out.println("day");
       
     }
 
-    public void day() {
+    public void day(String day) {
         System.out.println("낮에는 열심히 수업듣자");
     }
 
-    public void night() {
-      
+    public void night(String night) {
+      night = "밤";
     }
     
-    public void afternoon(){
+    public void afternoon(String afternoon){
         System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+        afternoon = "오후";
     }
 	
-    public void base() {
-    	System.out.printf();
-    }
+//    public void base() {
+//    	System.out.printf();
+//    }
 
   
 }
--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -6,7 +6,7 @@ public class FriendApp {
 
     public static void main(String[] args) {
 
-        Friend[] friendArray = new Friend[3];
+        Friend[] friendArr = new Friend[3];
         Scanner sc = new Scanner(System.in);
 
         System.out.println("친구를 3명 등록해 주세요");
@@ -14,14 +14,23 @@ public class FriendApp {
         
         //for문 시작
         //친구정보 3명 입력 로직 --> 반복문 사용
-            
-      
+        for(int i = 0; i < friendArr.length; i++) {
+        	System.out.print("이름: " );
+        	String name = sc.nextLine();
+        	System.out.print("핸드폰: " );
+        	String hp = sc.nextLine();
+        	System.out.print("학교: " );
+        	String school = sc.nextLine();
+        	
+        	friendArr[i] = new Friend (sc.name,sc.hp,sc.school);
+        }
         //for문 끝
         
         
         // 친구정보 출력
-        for (int i = 0; i < friendArray.length; i++) {
+        for (int i = 0; i < friendArr.length; i++) {
             //친구정보 출력 메소드 호출
+        	System.out.print("이름: " + friendArr[i].getName() + "핸드폰: " + friendArr[i].getHp() + "학교: " + friendArr[i].getSchool());
         }
 
         sc.close();
