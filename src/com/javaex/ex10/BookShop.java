35fb280eb357880df3daf102463867992287fc1f
PARK YESUNG <cjsrnr0504@naver.com>
2024-04-23 09:53:04 +0900

    24.04.23 학원

--- src/com/javaex/ex10/BookShop.java
+++ src/com/javaex/ex10/BookShop.java
@@ -28,13 +28,10 @@ public class BookShop {
 
         // (1) 입력된 번호에 맞는 책을 찾아 대여 되었음(상태코드=0)을 체크 합니다.
         // 코드작성
-        for(int i = 0; i < 10; i++) {
-        	books[i].books = newbooks;
-        	if() {
-        		books[i].rent();
-        	}
-        }
-        
+//        for(int i : books) {
+//        	
+//        }
+//        
         System.out.println(num + "이(가) 대여되었습니다.");
         
 
